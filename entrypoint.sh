bddbe7ca97eaa8edc17794315baec6f468226e80
Brian Wong <brian.wong@biai.com.hk>
2019-11-08 18:34:19 +0800

    Updated typo in endpoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,4 +9,4 @@ zipname = "site-packages.zip"
 aws configure set aws_access_key_id $1
 aws configure set aws_secret_access_key $2
 zip -a $zipname $GITHUB_WORKSPACE 
-aws lambda update-funnction-code --function-name $3 --zip-file $zipname
+aws lambda update-function-code --function-name $3 --zip-file $zipname
