ca8d9cab2beb2a0d760ad202f227489901bac941
Brian Wong <brian.wong@biai.com.hk>
2019-11-08 18:51:46 +0800

    Updated entrypoint.sh variable case

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@ if [ -z "$GITHUB_WORKSPACE" ]; then
   exit 1
 fi
 
-zipname = "site-packages.zip"
+ZIPNAME = "site-packages.zip"
 aws configure set aws_access_key_id $1
 aws configure set aws_secret_access_key $2
-zip -a $zipname $GITHUB_WORKSPACE 
-aws lambda update-function-code --function-name $3 --zip-file $zipname
+zip -a ZIPNAME $GITHUB_WORKSPACE 
+aws lambda update-function-code --function-name $3 --zip-file $ZIPNAME
