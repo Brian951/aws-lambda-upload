2ba2e21b4f694b0eca95e50ea0164ee01e20c163
Brian Wong <brian.wong@biai.com.hk>
2019-11-08 19:07:27 +0800

    Changed option in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,5 @@ fi
 ZIPNAME="site-packages.zip"
 aws configure set aws_access_key_id $1
 aws configure set aws_secret_access_key $2
-zip -a ZIPNAME $GITHUB_WORKSPACE 
+zip -r ZIPNAME $GITHUB_WORKSPACE 
 aws lambda update-function-code --function-name $3 --zip-file $ZIPNAME
