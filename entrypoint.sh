5f0b8e8d56463a4e16f2886f687ae4b82cfff964
Brian Wong <brian.wong@biai.com.hk>
2019-11-08 16:37:49 +0800

    First commit

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -l
+
+if [[ -z "$GITHUB_WORKSPACE" ]]; then
+  echo "Set the GITHUB_WORKSPACE env variable."
+  exit 1
+fi
+
+zipname = site-packages.zip
+aws configure set aws_access_key_id $1
+aws configure set aws_secret_access_key $2
+zip -a $zipname $GITHUB_WORKSPACE 
+aws lambda update-funnction-code --function-name nrdemo-test --zip-file zipname
