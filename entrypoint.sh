c79d532a77c265982fbfc2646d4cacfb4947bee1
Brian Wong <brian.wong@biai.com.hk>
2019-11-11 13:50:16 +0800

    ditto

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,6 @@ ZIPNAME="site-packages.zip"
 aws configure set aws_access_key_id $1
 aws configure set aws_secret_access_key $2
 aws configure set default.region $3
-cd $GITHUB_WORKSPACE 
-zip -r $ZIPNAME .
+zip -r $ZIPNAME ./$GITHUB_WORKSPACE/
 UPLOADNAME="fileb://$ZIPNAME"
 aws lambda update-function-code --function-name $4 --zip-file $UPLOADNAME
