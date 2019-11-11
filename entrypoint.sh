de9010332b4096a5a0be9af0b6773000da720774
Brian Wong <brian.wong@biai.com.hk>
2019-11-11 11:12:43 +0800

    Added a region variable for aws configure

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,7 @@ fi
 ZIPNAME="site-packages.zip"
 aws configure set aws_access_key_id $1
 aws configure set aws_secret_access_key $2
+aws configure set default.region $3
 zip -r $ZIPNAME $GITHUB_WORKSPACE 
 UPLOADNAME="fileb://$ZIPNAME"
-aws lambda update-function-code --function-name $3 --zip-file $UPLOADNAME
+aws lambda update-function-code --function-name $4 --zip-file $UPLOADNAME
