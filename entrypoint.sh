cc5f8dd03171557520853b91f0c9bd5cb5936395
Brian Wong <brian.wong@biai.com.hk>
2019-11-11 12:45:34 +0800

    Updated entry point to zip current directory, isntead of what might be a folder(?)

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,6 +13,7 @@ ZIPNAME="site-packages.zip"
 aws configure set aws_access_key_id $1
 aws configure set aws_secret_access_key $2
 aws configure set default.region $3
-zip -r $ZIPNAME $GITHUB_WORKSPACE 
+cd $GITHUB_WORKSPACE 
+zip -r $ZIPNAME .
 UPLOADNAME="fileb://$ZIPNAME"
 aws lambda update-function-code --function-name $4 --zip-file $UPLOADNAME
